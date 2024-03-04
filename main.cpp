54d9fac7d4b764a7087768552ec391c08df44780
avros16 <114757828+avros16@users.noreply.github.com>
2024-03-04 03:50:55 +0000

    design

--- main.cpp
+++ main.cpp
@@ -44,15 +44,16 @@ int main() {
   if (rules == "Y" || rules == "y") { //covers both Y and y. proud of this :3
     cout << " \n" << "*************************************************************************\n" << "\n"<< "⦿ We will give you 4 opportunities to guess the number correctly.\n" << "⦿ The number will range from 0 to 99.\n" <<"⦿ Every correct guess gets you 1 point.\n⦿ You need 2 points to win.\n⦿ The number will change after every round. \n⦿ Hints will be offered.\n \n" ;
   } else if (rules == "N" || rules == "n") {
-  cout << "\n"<< "Too cool for rules... we like that.\n";
+  cout << "\n"<< "Too cool for rules... we like that.\n\n";
   }
 
+  cout << "*************************************************************************\n\n";
   cout << "Are you ready (Y/N)? ";
   cin >> rules; 
-
+  cout << "\n\n*************************************************************************\n\n";
 
   if (rules == "Y" || rules == "y") {
-    cout << "\n  Good luck!\n";
+    cout << "\n Good luck!\n\n";
     while (chances <5 && score <1) {
       chances++;
     //cout << random_number;
@@ -72,21 +73,23 @@ int main() {
           }
 
     } else if (guess == random_number) {
-      cout << "Correct!\n";
+      cout << "Correct!\n\n";
       score++;
-      break;} //ends else if 
+      break; 
+    
+   } //ends else if 
     } // ends if on line  60
 
-
+    cout << "*************************************************************************\n\n";
 cout << "Your current score is: " << score <<".\n";
 cout << "You need 2 points to win. Do you wanna keep playing? The number will change (Y/N) ";
   
   
     cin >> rules; 
-
+    cout << "\n\n*************************************************************************\n";
 
     if (rules == "Y" || rules == "y") {
-      cout << "\n  Good luck!\n";
+      cout << "\nGood luck!\n\n";
       while (chances2 <5 && score <2) {
         chances2++;
       //cout << random_number2;
@@ -106,21 +109,21 @@ cout << "You need 2 points to win. Do you wanna keep playing? The number will ch
             }
 
       } else if (guess == random_number2) {
-        cout << "Correct!\n";
+        cout << "Correct!\n\n";
         score++;
         break;} 
       } // 
-
+      cout << "\n\n*************************************************************************\n\n";
       if (score == 2) {
         cout <<  "Your current score is: " << score <<".\n" << "Congradulations! You won the game! you are a guessing champion!"; 
       } else if (score <2) {
         cout << "Your current score is: " << score <<".\n";
-        cout << "This is your last chance! Would you like to play the final round? (Y/N) ";
+        cout << "This is your last chance! We will double the points so you can win big! Would you like to play the final round? (Y/N) ";
         cin >> rules; 
 
 
         if (rules == "Y" || rules == "y") {
-          cout << "\n  Good luck!\n";
+          cout << "\n Good luck!\n";
           while (chances3 <5 && score <3) {
             chances3++;
           //cout << random_number3;
@@ -140,7 +143,8 @@ cout << "You need 2 points to win. Do you wanna keep playing? The number will ch
                 }
 
           } else if (guess == random_number3) {
-            cout << "Correct!\n";
+            cout << "Correct!\n\n";
+            score++;
             score++;
             break;} 
         
@@ -154,12 +158,23 @@ cout << "You need 2 points to win. Do you wanna keep playing? The number will ch
 
 
 // do they wanna know what the numbers were?
+    cout << "\n\n*************************************************************************\n\n";
 
-cout << "The numbers were: "<< random_number << ", " << random_number2 << ", " << random_number3 << "\n";
+if (score <2) {
+  cout << "Better luck next time! ";
+} else if (score >=2) {
+  cout << "Congradulations! You won the game! you are a guessing champion! \n\n"; 
+}
 
-    cout << "Thanks for playing!";
 
+    
+cout << "The numbers were: "<< random_number << ", " << random_number2 << ", " << random_number3 << "\n\n";
 
+    cout << "                        Thanks for playing!";
+
+    cout << "\n\n*************************************************************************\n\n";
   }
+
+ 
 
 } //ends main function 
